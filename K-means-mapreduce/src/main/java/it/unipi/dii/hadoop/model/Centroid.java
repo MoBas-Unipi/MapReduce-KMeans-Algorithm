2bd54f1c496e340a271279ba60a2e5613f199c38
LucT3 <luc.tart3@gmail.com>
2023-09-29 19:09:34 +0200

    update Centroid class
    -defined the functions to serialize/deserialize centroid objects.
    -defined function to compare 2 keys (IntWritable types).

--- K-means-mapreduce/src/main/java/it/unipi/dii/hadoop/model/Centroid.java
+++ K-means-mapreduce/src/main/java/it/unipi/dii/hadoop/model/Centroid.java
@@ -6,7 +6,6 @@ import org.apache.hadoop.io.WritableComparable;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
-import java.util.List;
 
 public class Centroid implements WritableComparable<Centroid> {
 
@@ -27,23 +26,53 @@ public class Centroid implements WritableComparable<Centroid> {
         return point;
     }
 
+    /**
+     * Override IntWritable class compareTo function.
+     * function used for the shuffle and sort operation.
+     * compares two centroid ids and returns the following values based on the result:
+     *  -1: if the currentCentroidID is less than the centroidID passed as parameter
+     *   0: if they are equal
+     *   1: if the currentCentroidID is greater than the centroidID passed as parameter
+     * @param centroid Centroid object to compare against the current centroid
+     * @return an integer based on the comparison result (-1, 0 or 1)
+     */
     @Override
     public int compareTo(Centroid centroid) {
-        return 0;
+        //initialize the centroid IDs to compare
+        int currentCentroidID = Integer.parseInt(String.valueOf(this.centroidID));
+        int centroidIDtoCompare = Integer.parseInt(String.valueOf(centroid.getCentroidID()));
+
+        //compare the centroid IDs and return a value as following
+        return (currentCentroidID < centroidIDtoCompare ? -1 : (currentCentroidID == centroidIDtoCompare ? 0 : 1));
+
     }
 
+    /**
+     * Override IntWritable class write function.
+     * serialize in bytes the centroidID and the point object (having the centroid coordinates) attributes of the centroid
+     * @param dataOutput output stream object
+     * @throws IOException
+     */
     @Override
     public void write(DataOutput dataOutput) throws IOException {
+        //serialize the centroid id
+        dataOutput.writeInt(this.centroidID.get());
 
+        //serialize the point object (custom object)
+        this.point.write(dataOutput);
     }
 
+    /**
+     * Override Writable class readFields function
+     * deserialize from bytes into a Centroid object attributes the centroidID and the point object (having the centroid coordinates)
+     * @param dataInput input stream object
+     * @throws IOException
+     */
     @Override
     public void readFields(DataInput dataInput) throws IOException {
-
+        //deserialize the centroid id and the point object
+        this.centroidID.readFields(dataInput);
+        this.point.readFields(dataInput);
     }
-
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
+    
 }
